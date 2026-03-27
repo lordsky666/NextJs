da590c53d99e64a9d3557d58167feb1d7fe9b7c9
lordsky666 <avramrobert2060@gmail.com>
2026-03-27 14:58:15 +0200

    Top menu v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
+import { Urbanist } from "next/font/google";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-      <div className="H1">
-        <h1>Salut! Aceasta este prima mea pagină Next.js</h1>
-      </div>
+      <div className="Top">
+        <Image src="/Foto/image_1.png"         
+        alt="Imaginea federatiei"
+        width={100} 
+        height={100}
+        />
 
-      <div>
-        <button>"Im button"</button>
+        <div className="Top-menu">
+          <a href="#home">Pagina principala</a>
+          <a href="#home">Campioante</a>
+          <a href="#home"></a>
+        </div>
       </div>
+    
+      {/* <div>
+        <button>"Im button"</button>
+      </div> */}
 
       <div className="Describe">
         <p>Write your own project</p>
       </div>
 
 
-      
+
     </div>
 
   );
