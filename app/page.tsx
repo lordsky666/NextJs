c8e67a8595b1b457d429b616601f38889e4768c6
lordsky666 <avramrobert2060@gmail.com>
2026-03-27 15:44:41 +0200

    Partea de sus gata

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,35 @@
-import { Urbanist } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Image from "next/image";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: "500" });
 
 export default function Home() {
   return (
-    <div>
+    <div className={montserrat.className}>
       <div className="Top">
-        <Image src="/Foto/image_1.png"         
-        alt="Imaginea federatiei"
-        width={100} 
-        height={100}
+        <Image
+          src="/Foto/image_1.png"
+          alt="Logo Federației"
+          width={100}
+          height={100}
         />
 
-        {/* <div className="Top-menu">
-          <a href="#home">Pagina principala</a>
-          <a href="#home">Campioante</a>
-          <a href="#home"></a>
-        </div> */}
+        <div className="Top-menu">
+          <a href="#home">Acasă</a>
+          <a href="#champions">Campionate</a>
+          <a href="#news">Noutăți</a>
+        </div>
       </div>
 
-      {/* <div>
-        <button>"Im button"</button>
-      </div> */}
-
-      <div className="Describe">
-        <p>Write your own project</p>
+      <div className="Main_Content">
+      <Image
+      src="/Foto/wea.jpg"
+      alt="Poza de background"  
+      layout="responsive"
+      width={16}
+      height={9}
+      />
       </div>
-
-
-
     </div>
-
   );
 }
